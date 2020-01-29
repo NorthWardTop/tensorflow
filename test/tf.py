ff14b597153641ecb3de70d5ad48558810e797d8
NorthWardTop <2394783277@qq.com>
2020-01-29 21:53:22 +0800

    首次提交

---
+++ test/tf.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# 三层模型
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# 编译模型
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# 喂模型, 为了模型适合训练集
+model.fit(train_images, train_labels, epochs=10)
+
+# 使用测试集评估模型
+# test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+# print('\nTest: ', test_acc)
+
+predictions = model.predict(test_images)
+print(predictions[0])
