ceaf72ff060f1c2b942c6281d490fe1bd5443e09
NorthWardTop <2394783277@qq.com>
2020-01-30 13:46:56 +0800

    sync

--- test/tf.py
+++ test/tf.py
@@ -10,13 +10,48 @@ from tensorflow import keras
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100*np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array, true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
 
 print(tf.__version__)
 
 
 fashion_mnist = keras.datasets.fashion_mnist
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+(train_images, train_labels), (test_images,
+                               test_labels) = fashion_mnist.load_data()
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
@@ -45,5 +80,27 @@ model.fit(train_images, train_labels, epochs=10)
 # test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 # print('\nTest: ', test_acc)
 
+# 预测测试集图像
 predictions = model.predict(test_images)
-print(predictions[0])
+# 图形化查看第0个测试集图像预测结果
+# i = 3
+# plt.figure(figsize=(6, 3))
+# plt.subplot(1, 2, 1)
+# plot_image(i, predictions[i], test_labels, test_images)
+# plt.subplot(1, 2, 2)
+# plot_value_array(i, predictions[i], test_labels)
+# plt.show()
+
+# 同上, 为单个图像预测
+# img = test_images[1]
+img = mpimg.imread('/media/lee/work-334GB/python/tensorflow/test/img.png')
+img = (np.expand_dims(img, 0))
+# 预测
+predictions_single = model.predict(img)
+plot_value_array(1, predictions_single[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+plt.show()
+
+
+
+
